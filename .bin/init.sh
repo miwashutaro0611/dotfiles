6da03940726bc99bbff926dc70eb0c470b6a6237
miwashutaro0611 <jacksumiwa0061@gmail.com>
2022-10-23 18:05:37 +0900

    feat: zsh-autosuggestions

--- .bin/init.sh
+++ .bin/init.sh
@@ -10,3 +10,7 @@ xcode-select --install > /dev/null
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
